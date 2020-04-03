423c0bf19b4e3f9b55d5c7a48936321b0013fff0
El Harag <elbenkrizi@gmail.com>
2020-04-03 05:55:50 +0200

    add

---
+++ main.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+import argparse
+import json
+import re
+
+def verify_file_conf(path_file):
+    with open(path_file) as file:
+        re_p = re.compile("(\[\w*\])")
+        re_c = re.compile("(^\w.*)=(.*)")
+        list_args_file = file.read().splitlines()
+        args = {}
+        name_p = ""
+        for arg in list_args_file:
+            if (re_p.match(arg)):
+                name_p = re_p.match(arg).group().strip("[").strip("]")
+                args[name_p] = {name_p: name_p}
+            elif (re_c.match(arg)):
+                l_tmp = list(filter(None, re_c.split(arg)))
+                key_c, value_c = l_tmp[0], l_tmp[1]
+                args[name_p][key_c] = value_c.split(" ")[0]
+        return (args)
+
+
+
+    
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog="taskmaster",
+        description="Superviser des programmes",
+        epilog="Programme servant à superviser les porgrammes donnée"
+    )
+    parser.add_argument(
+        "-c",
+        default="/etc/taskmaster.conf",
+        help="path file of [taskmaster.conf] by default is %(default)s",
+        metavar=""
+    )
+    args = parser.parse_args()
+    print("icicicii {}".format(args.c))
+    conf = verify_file_conf(args.c)
